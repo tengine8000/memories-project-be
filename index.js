221af99026070f0aec60fca17e09d44808195229
tengine8000 <torty.emmanuel@gmail.com>
2021-08-17 16:06:20 +0100

    Setup app for deployment

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => res.send('Hello there! Welcome to Memories Project API!'));
+
 const PORT = process.env.PORT || 5000; 
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
